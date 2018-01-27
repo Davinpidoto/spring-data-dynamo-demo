f3fba9e94c4ab6fb78e4e16bf07d3fc8729dc532
Davin Pidoto <davdog@me.com>
2018-01-27 19:33:01 +1100

    add user controller for crud operations

---
+++ src/main/java/com/davdog/dynamo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.davdog.dynamo.controller;
+
+import com.davdog.dynamo.controller.json.UserRequest;
+import com.davdog.dynamo.repositories.User;
+import com.davdog.dynamo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/users")
+  public Iterable<User> getUsers() {
+    return userRepository.findAll();
+  }
+
+  @PostMapping("/users")
+  public User createUser(@Valid @RequestBody UserRequest user) {
+    return userRepository.save(new User(user.getFirstName(), user.getLastName()));
+  }
+
+  @PutMapping("/users/{id}")
+  public User updateUser(@PathVariable String id, @Valid @RequestBody UserRequest user) {
+    return userRepository.save(new User(id, user.getFirstName(), user.getLastName()));
+  }
+
+  @DeleteMapping("/users/{id}")
+  public void deleteUser(@PathVariable String id) {
+    userRepository.deleteById(id);
+  }
+
+}
---
+++ src/main/java/com/davdog/dynamo/controller/json/UserRequest.java
@@ -0,0 +1,9 @@
+package com.davdog.dynamo.controller.json;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+  private String firstName;
+  private String lastName;
+}
