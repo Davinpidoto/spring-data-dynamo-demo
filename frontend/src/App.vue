f22d1d293fe6f05ee716a9effbbd246a1743992a
Davin Pidoto <davdog@me.com>
2018-01-28 11:16:30 +1100

    seperate front and back end into difernet folders

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="navbar navbar-dark bg-dark box-shadow">
+      <div class="container d-flex justify-content-between">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <strong>Spring Data DynamoDB</strong>
+        </a>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+  .navbar{
+    margin-bottom: 30px;
+  }
+</style>
---
+++ frontend/src/components/Users.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container" id="vue">
+    <h3 class="topHeader">Users</h3>
+    <div class="panel panel-default">
+      <table class="table table-striped table-bordered">
+        <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td >{{user.firstName}} {{user.lastName}}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    let self = this
+    axios.get('http://localhost:8080/users')
+      .then(function (response) {
+        self.users = response.data
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style scoped>
+  .panel{
+    margin-top:20px;
+  }
+</style>
